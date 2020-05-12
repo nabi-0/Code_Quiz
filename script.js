59f8d585c065cadcfdd7639d00345583fbc7dc70
Natalia Arias <abram363@hotmail.com>
2020-05-11 21:37:34 -0500

    start btn?

---
+++ script.js
@@ -0,0 +1,21 @@
+//Assign start button
+var startButton = document.getElementById("start-btn")
+
+startButton.addEventListener("click", startQuiz)
+// Starting the quiz/ pressing start button
+function startQuiz() {
+    console.log('Sarted')
+    startButton.classList.add("hide")
+}
+
+//Going to next question/ pressing the next button
+function setNextQuestion () {
+
+
+}
+
+//After selecting an answer/
+function selectAnswer () {
+
+
+}
